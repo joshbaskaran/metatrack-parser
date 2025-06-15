f0883aaa700d72bd393a136972f2ff397564a5e1
Joshua Baskaran <joshbaskaran@gmail.com>
2025-06-15 21:40:31 +0200

    chore: remove fluff
    Note to self: This is not for production, no need for ceremonies; just hack away

--- pseudomonas_unn_parser.py
+++ pseudomonas_unn_parser.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pprint
 
 import httpx
 from dotenv import load_dotenv
@@ -37,78 +36,11 @@ def sign_out():
     sign_out_req.raise_for_status()
 
 
-# create pseudomonas project
-def create_pseudomans_project():
-    # read all projects
-    projects = client.get(f"{API_BASE_URL}/projects/")
+def main():
+    sing_in()
 
-    if not projects.json():
-        create_project = client.post(
-            f"{API_BASE_URL}/projects/",
-            headers={"Content-Type": "application/json"},
-            json={"name": "pseudomonas", "description": "pseudomonas data from UNN"},
-        )
-        create_project.raise_for_status()
-        return create_project.json()
-
-    if projects.json():
-        # check if pseudomonas already exists
-        for proj in projects.json():
-            if proj["name"] == "pseudomonas":
-                return proj
-
-        create_project = client.post(
-            f"{API_BASE_URL}/projects/",
-            headers={"Content-Type": "application/json"},
-            json={
-                "name": "pseudomonas",
-                "description": "pseudomonas data from UNN",
-            },
-        )
-        create_project.raise_for_status()
-        return create_project.json()
-    return {}
-
-
-def create_investigation_if_not_exists(project_id):
-    investigations = client.get(f"{API_BASE_URL}/projects/{project_id}/investigations/")
-    investigations.raise_for_status()
-
-    def create_investigation():
-        invest = client.post(
-            f"{API_BASE_URL}/projects/{project_id}/investigations/",
-            headers={"Content-Type": "application/json"},
-            json={
-                "filename": "pseudomonas_unn.xlsx",
-                "identifier": "pseudomonas",
-                "title": "pseudomonas",
-                "submissionDate": "",
-                "publicReleaseDate": "",
-                "ontologySourceReferences": "",
-                "publications": [""],
-                "people": [""],
-                "studies": [""],
-            },
-        )
-        invest.raise_for_status()
-        return invest.json()
-
-    if not investigations.json():
-        return create_investigation()
-
-    for inv in investigations.json():
-        if inv["identifier"] == "pseudomonas":
-            return inv
-
-    return create_investigation()
+    sign_out()
 
 
 if __name__ == "__main__":
-    sing_in()
-
-    project = create_pseudomans_project()
-    investigation = create_investigation_if_not_exists(project["id"])
-
-    pprint(investigation)
-
-    sign_out()
+    main()
